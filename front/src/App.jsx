78a97a8c0d7128b4b6a83f706b3e22076a8bbf33
Loan <loan.thomy@gmail.com>
2025-01-24 13:33:39 +0100

    commandes page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import ProductDetails from "./pages/Detail_Produit"; // Import de la page des détails du produit
 import Panier from "./pages/Panier"; // Import de la page des détails du produit
+import Commandes from "./pages/Commandes"; // Import de la page Commandes
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} /> {/* Route vers la page d'accueil */}
         <Route path="/produit/:id" element={<ProductDetails />} /> {/* Route pour afficher les détails du produit */}
         <Route path="/panier" element={<Panier />} />
+        <Route path="/commandes" element={<Commandes />} />
       </Routes>
     </Router>
   );
